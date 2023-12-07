561955cc1476001b072ae4d4ecaf32cfa5680f7c
Basma2023 <basma.sayyad@gmail.com>
2023-12-06 22:17:26 -0500

    Generate HTML elements from JSON data

--- js/main-json.js
+++ js/main-json.js
@@ -3,9 +3,10 @@ const profilesContainer = document.getElementById("profilesContainer");
 fetch("json/dataset.json")
   .then((response) => response.json())
   .then((jsonData) => {
+// Loop through the JSON data and create HTML elements
+jsonData.forEach((profile) => {
 
 
 
-    
   }
 
