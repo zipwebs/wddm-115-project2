d34808f1d0fd3975407d17ce05c7627d4db0a6fe
Basma2023 <basma.sayyad@gmail.com>
2023-12-06 22:23:58 -0500

    feat: Append profile divs to container This commit appends the dynamically created profile divs to the profiles container in the HTML. The profilesContainer.appendChild(profileDiv) line ensures seamless integration of the profile cards into the UI.

--- js/main-json.js
+++ js/main-json.js
@@ -1,16 +1,16 @@
 const profilesContainer = document.getElementById("profilesContainer");
 // Fetch the JSON file
 fetch("json/dataset.json")
-  .then((response) => response.json())
-  .then((jsonData) => {
-// Loop through the JSON data and create HTML elements
-jsonData.forEach((profile) => {
+    .then((response) => response.json())
+    .then((jsonData) => {
+        // Loop through the JSON data and create HTML elements
+        jsonData.forEach((profile) => {
 
-// Create a div element for each profile
-const profileDiv = document.createElement("div");
-profileDiv.classList.add("profile");
-// Create and set the inner HTML for the profile
-profileDiv.innerHTML = `
+            // Create a div element for each profile
+            const profileDiv = document.createElement("div");
+            profileDiv.classList.add("profile");
+            // Create and set the inner HTML for the profile
+            profileDiv.innerHTML = `
 
 <div class="col-sm-6 col-lg-3 col-12 inner-profile-item">
 <div class="card  card-main">
@@ -25,5 +25,12 @@ profileDiv.innerHTML = `
 </div>
 
 `;
-  }
+            // Append the profile div to the container
+            profilesContainer.appendChild(profileDiv);
+        });
+    })
+    .catch((error) => console.error("Error fetching JSON:", error));
+
+
+  
 
