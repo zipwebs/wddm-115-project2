c6823276ae4e10213796e489d3dc3993bc9637bd
Basma2023 <basma.sayyad@gmail.com>
2023-12-06 22:37:25 -0500

    Add duplicateMe() function to clone and  append a button

--- js/main.js
+++ js/main.js
@@ -34,3 +34,17 @@ function toggleSectionHeadingDecoration() {
         element.classList.add('normal');
     }
 }
+function duplicateMe() {
+    const buttonToDuplicate = document.getElementById('duplicateMe');
+
+    if (buttonToDuplicate) {
+        // Create a copy of the button
+        const clonedButton = buttonToDuplicate.cloneNode(true);
+
+        // Optional: Give the cloned button a new ID or remove the ID to avoid duplicate IDs
+        clonedButton.removeAttribute('id');
+
+        // Append the cloned button after the original button
+        buttonToDuplicate.parentNode.insertBefore(clonedButton, buttonToDuplicate.nextSibling);
+    }
+}
