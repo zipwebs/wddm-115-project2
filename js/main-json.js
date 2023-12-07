b45c9bbe527d9bf97a793c5834c7607e0241fea8
Basma2023 <basma.sayyad@gmail.com>
2023-12-06 22:15:32 -0500

    This commit includes changes to the "dataset.json"  file. The data has been updated/added using the fetch method

--- js/main-json.js
+++ js/main-json.js
@@ -1,1 +1,11 @@
 const profilesContainer = document.getElementById("profilesContainer");
+// Fetch the JSON file
+fetch("json/dataset.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+
+
+
+    
+  }
+
