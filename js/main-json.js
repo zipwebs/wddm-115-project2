27c0040283eb60dbd1d88b9e8236d88cc607d315
Basma2023 <basma.sayyad@gmail.com>
2023-12-06 22:21:07 -0500

    Set inner HTML for profile cards
    This commit includes code to dynamically generate HTML content for each profile. A div element with the class "inner-profile-item" is created, containing a card structure displaying profile information such as name, age, and address. The "Invite To Travel" button is also added for user interaction.

--- js/main-json.js
+++ js/main-json.js
@@ -9,6 +9,21 @@ jsonData.forEach((profile) => {
 // Create a div element for each profile
 const profileDiv = document.createElement("div");
 profileDiv.classList.add("profile");
+// Create and set the inner HTML for the profile
+profileDiv.innerHTML = `
 
+<div class="col-sm-6 col-lg-3 col-12 inner-profile-item">
+<div class="card  card-main">
+<img src="images/profiles/${profile.picture}" class="card-img-top" alt="...">
+<div class="card-body">
+<h2>${profile.firstName} ${profile.lastName}</h2>
+  <p>Age: ${profile.age}</p>
+  <p class="address">Address: ${profile.address.street}, ${profile.address.city}, ${profile.address.country}</p>
+  <a href="#" class="btn btn-primary">Invite To Travel</a>
+</div>
+</div>
+</div>
+
+`;
   }
 
